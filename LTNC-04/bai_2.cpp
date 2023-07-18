3568fbc3729b7ff6470e45fa09bdd7ae228ddca9
Trần Duy Toàn <toantranx17ht@gmail.com>
2023-07-18 10:21:43 +0700

    2

---
+++ LTNC-04/bai_2.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'equalizeArray' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+int equalizeArray(vector<int> arr) {
+    int max = 0;
+    vector<int> list;
+    for (int i = 0; i < arr.size(); i++) {
+        //ktra xem da tung xuat hien chua
+        bool check = true;
+        for (int l : list) {
+            if (arr[i] == l) {
+                check = false;
+                break;
+            }
+        }
+        if (check) {
+            list.push_back(arr[i]);
+            //dem so lan xuat hien trong arr
+            int count = 0;
+            for (int j = i; j < arr.size(); j++) {
+                if (arr[j] == arr[i]) count++;
+            }
+            if (count > max) max = count;
+        } else continue;
+    }
+    return arr.size()-max;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = equalizeArray(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
