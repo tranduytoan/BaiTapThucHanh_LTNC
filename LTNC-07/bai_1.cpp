b12d3a9ef0798d9381a285c84c7297e4ca46be50
Trần Duy Toàn <toantranx17ht@gmail.com>
2023-07-29 18:50:20 +0700

    7.1

---
+++ LTNC-07/bai_1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool binarySearh(vector<int> a, int left, int right, int x) {
+    if (left <= right) {
+        int mid = (left + right) / 2;
+        if (a[mid] == x) return true;
+        if (a[mid] > x) return binarySearh(a, left, mid - 1, x);
+        return binarySearh(a, mid + 1, right, x);
+    }
+    return false;
+}
+
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    if (binarySearh(a, 0, n - 1, x)) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
